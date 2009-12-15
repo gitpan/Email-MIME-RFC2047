31f5e8610417bf174d1fb1b8a1f7ea4e42fbef5e
Nick Wellnhofer <nwellnhof@cpan.org>
2009-12-15 01:08:44 +0000

    Import of NWELLNHOF/Email-MIME-RFC2047-0.89_01 from CPAN.
    gitpan-cpan-distribution: Email-MIME-RFC2047
    gitpan-cpan-version:      0.89_01
    gitpan-cpan-path:         NWELLNHOF/Email-MIME-RFC2047-0.89_01.tar.gz
    gitpan-cpan-author:       NWELLNHOF
    gitpan-cpan-maturity:     developer

---
+++ lib/Email/MIME/RFC2047/Decoder.pm
@@ -0,0 +1,213 @@
+package Email::MIME::RFC2047::Decoder;
+our $VERSION = '0.89_01';
+
+use strict;
+
+use Encode ();
+use MIME::Base64 ();
+
+# Regex for encoded words including leading whitespace.
+# This also checks the validity of base64 encoded data because MIME::Base64
+# silently ignores invalid characters.
+# Captures ($ws, $encoding, $content_b, $content_q)
+my $encoded_word_re = qr/
+    ( ^ | \s+ )
+    = \? ( [\w-]+ ) \?
+    (?:
+        [Bb] \?
+        (
+            (?:
+                [A-Za-z0-9+\/]{2}
+                (?: == | [A-Za-z0-9+\/] [A-Za-z0-9+\/=] )
+            )+
+        ) |
+        [Qq] \?
+        ( [^?\x00-\x20\x7f-\x{ffff}]+ )
+    )
+    \? =
+    (?= \z | \s+ )
+/x;
+
+my $quoted_string_re = qr/
+    "
+    (
+        (?:
+            [^"\\] |
+            \\ .
+        )*
+    )
+    "
+/sx;
+
+sub new {
+    my $package = shift;
+
+    my $self = {};
+
+    return bless($self, $package);
+}
+
+sub decode_text {
+    my ($self, $encoded) = @_;
+
+    return $self->_decode('text', $encoded);
+}
+
+sub decode_phrase {
+    my ($self, $encoded) = @_;
+
+    return $self->_decode('phrase', $encoded);
+}
+
+sub _decode {
+    my ($self, $mode, $encoded) = @_;
+
+    my $result = '';
+    my $pos = 0;
+    my $enc_flag;
+    my $regex = $mode eq 'phrase' ?
+        qr/$encoded_word_re|$quoted_string_re/ :
+        $encoded_word_re;
+
+    while($encoded =~ /\G(.*?)($regex)/gs) {
+        my ($text, $match,
+            $ws, $encoding, $b_content, $q_content,
+            $qs_content) =
+            ($1, $2, $3, $4, $5, $6, $7);
+        $pos = pos($encoded);
+
+        $result .= $text;
+
+        if(defined($encoding)) {
+            # encoded words shouldn't be longer than 75 chars but
+            # let's allow up to 255 chars
+            if(length($match) - length($ws) > 255) {
+                $result .= $match;
+                $enc_flag = undef;
+                last;
+            }
+
+            my $content;
+
+            if(defined($b_content)) {
+                # MIME B
+                $content = MIME::Base64::decode_base64($b_content);
+            }
+            else {
+                # MIME Q
+                $content = $q_content;
+                $content =~ tr/_/ /;
+                $content =~ s/=([0-9A-Fa-f]{2})/chr(hex($1))/eg;
+            }
+
+            my $chunk;
+            eval {
+                $chunk = Encode::decode(
+                    $encoding,
+                    $content,
+                    Encode::FB_CROAK
+                );
+            };
+
+            if($@) {
+                warn($@);
+                # display raw encoded word in case of errors
+                $result .= $match;
+                $enc_flag = undef;
+                last;
+            }
+
+            # ignore whitespace between encoded words
+            $result .= $ws unless $enc_flag && $text eq '';
+
+            $result .= $chunk;
+
+            $enc_flag = 1;
+        }
+        else {
+            # quoted string, unquote
+            $qs_content =~ s/\\(.)/$1/gs;
+            $result .= $qs_content;
+
+            $enc_flag = undef;
+        }
+    }
+
+    $result .= substr($encoded, $pos);
+
+    # normalize    
+    $result =~ s/^\s+//;
+    $result =~ s/\s+\z//;
+    $result =~ s/\s+/ /g;
+
+    # remove potentially dangerous ASCII control chars
+    $result =~ s/[\x00-\x1f\x7f]//g;
+
+    return $result;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Email::MIME::RFC2047::Decoder - Decoding of non-ASCII MIME headers
+
+=head1 SYNOPSIS
+
+ use Email::MIME::RFC2047::Decoder;
+ 
+ my $decoder = Email::MIME::RFC2047::Decoder->new();
+ 
+ my $string = $decoder->decode_text($encoded_text);
+ my $string = $decoder->decode_phrase($encoded_phrase);
+
+=head1 DESCRIPTION
+
+This module decodes parts of MIME email message headers containing non-ASCII
+text according to RFC 2047.
+
+=head1 CONSTRUCTOR
+
+=head2 new
+
+ my $decoder = Email::MIME::RFC2047::Decoder->new();
+
+Creates a new decoder object.
+
+=head1 METHODS
+
+=head2 decode_text
+
+ my $string = $decoder->decode_text($encoded_text);
+
+Decodes any MIME header field for which the field body is defined as '*text'
+(as defined by RFC 822), for example, any Subject or Comments header field.
+
+The resulting string is trimmed and any whitespace is collapsed.
+
+=head2 decode_phrase
+
+ my $string = $decoder->decode_phrase($encoded_phrase);
+
+Decodes any 'phrase' token (as defined by RFC 822) in a MIME header field,
+for example, one that precedes an address in a From, To, or Cc header.
+
+This method works like I<decode_text> but additionally unquotes any
+'quoted-strings'.
+
+=head1 AUTHOR
+
+Nick Wellnhofer <wellnhofer@aevum.de>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) Nick Wellnhofer, 2009
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
+
---
+++ lib/Email/MIME/RFC2047/Encoder.pm
@@ -0,0 +1,264 @@
+package Email::MIME::RFC2047::Encoder;
+our $VERSION = '0.89_01';
+
+use strict;
+
+use Encode ();
+use MIME::Base64 ();
+
+sub new {
+    my $package = shift;
+    my $options = ref($_[0]) ? $_[0] : { @_ };
+
+    my ($encoding, $method) = ($options->{encoding}, $options->{method});
+
+    if(!defined($encoding)) {
+        $encoding = 'utf-8';
+        $method = 'Q' if !defined($method);
+    }
+    else {
+        $method = 'B' if !defined($method);
+    }
+
+    my $encoder = Encode::find_encoding($encoding)
+        or die("encoding '$encoding' not found");
+
+    my $self = {
+        encoding => $encoding,
+        encoder  => $encoder,
+        method   => uc($method),
+    };
+
+    return bless($self, $package);
+}
+
+sub encode_text {
+    my ($self, $string) = @_;
+
+    return $self->_encode('text', $string);
+}
+
+sub encode_phrase {
+    my ($self, $string) = @_;
+
+    return $self->_encode('phrase', $string);
+}
+
+sub _encode {
+    my ($self, $mode, $string) = @_;
+
+    my $encoder = $self->{encoder};
+    my $result = '';
+
+    # $string is split on whitespace. Each $word is categorized into
+    # 'mime', 'quoted' or 'text'. The intermediate result of the conversion of
+    # consecutive words of the same types is accumulated in $buffer.
+    # The type of the buffer is tracked in $buffer_type.
+    # The method _finish_buffer is called to finish the encoding of the
+    # buffered content and append to the result.
+    my $buffer = '';
+    my $buffer_type;
+
+    for my $word (split(/\s+/, $string)) {
+        next if $word eq ''; # ignore leading white space
+
+        $word =~ s/[\x00-\x1f\x7f]//g; # better remove control chars
+
+        my $word_type;
+
+        if($word =~ /[\x80-\x{ffff}]|(^=\?.*\?=\z)/s) {
+            # also encode any word that starts with '=?' and ends with '?='
+            $word_type = 'mime';
+        }
+        elsif($mode eq 'phrase') {
+            $word_type = 'quoted';
+        }
+        else {
+            $word_type = 'text';
+        }
+        
+        $self->_finish_buffer(\$result, $buffer_type, \$buffer)
+            if $buffer ne '' && $buffer_type ne $word_type;
+        $buffer_type = $word_type;
+
+        if($word_type eq 'text') {
+            $result .= ' ' if $result ne '';
+            $result .= $word;
+        }
+        elsif($word_type eq 'quoted') {
+            $buffer .= ' ' if $buffer ne '';
+            $buffer .= $word;
+        }
+        else {
+            my $max_len = 75 - 7 - length($self->{encoding});
+            $max_len = 3 * ($max_len >> 2) if $self->{method} eq 'B';
+
+            my @chars;
+            push(@chars, ' ') if $buffer ne '';
+            push(@chars, split(//, $word));
+
+            for my $char (@chars) {
+                my $chunk;
+                
+                if($self->{method} eq 'B') {
+                    $chunk = $encoder->encode($char);
+                }
+                elsif($char =~ /[()<>@,;:\\".\[\]=?_]/) {
+                    # special character
+                    $chunk = sprintf('=%02x', ord($char));
+                }
+                elsif($char =~ /[\x80-\x{ffff}]/) {
+                    # non-ASCII character
+
+                    my $enc_char = $encoder->encode($char);
+                    $chunk = '';
+                    
+                    for my $byte (unpack('C*', $enc_char)) {
+                        $chunk .= sprintf('=%02x', $byte);
+                    }
+                }
+                elsif($char eq ' ') {
+                    $chunk = '_';
+                }
+                else {
+                    $chunk = $char;
+                }
+
+                if(length($buffer) + length($chunk) <= $max_len) {
+                    $buffer .= $chunk;
+                }
+                else {
+                    $self->_finish_buffer(\$result, 'mime', \$buffer);
+                    $buffer = $chunk;
+                }
+            }
+        }
+    }
+
+    $self->_finish_buffer(\$result, $buffer_type, \$buffer)
+        if $buffer ne '';
+
+    return $result;
+}
+
+sub _finish_buffer {
+    my ($self, $result, $buffer_type, $buffer) = @_;
+
+    $$result .= ' ' if $$result ne '';
+
+    if($buffer_type eq 'quoted') {
+        if($$buffer =~ /[()<>@,;:\\".\[\]]/) {
+            # use quoted string if buffer contains special chars
+            $$buffer =~ s/[\\"]/\\$&/g;
+            
+            $$result .= qq("$$buffer");
+        }
+        else {
+            $$result .= $$buffer;
+        }
+    }
+    elsif($buffer_type eq 'mime') {
+        $$result .= "=?$self->{encoding}?$self->{method}?";
+
+        if($self->{method} eq 'B') {
+            $$result .= MIME::Base64::encode_base64($$buffer, '');
+        }
+        else {
+            $$result .= $$buffer;
+        }
+
+        $$result .= '?=';
+    }
+
+    $$buffer = '';
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Email::MIME::RFC2047::Encoder - Encoding of non-ASCII MIME headers
+
+=head1 SYNOPSIS
+
+ use Email::MIME::RFC2047::Encoder;
+ 
+ my $encoder = Email::MIME::RFC2047::Encoder->new(
+     encoding => 'utf-8',
+     method   => 'Q',
+ );
+ 
+ my $encoded_text   = $encoder->encode_text($string);
+ my $encoded_phrase = $encoder->encode_phrase($string);
+
+=head1 DESCRIPTION
+
+This module encodes non-ASCII text for MIME email message headers according to
+RFC 2047.
+
+=head1 CONSTRUCTOR
+
+=head2 new
+
+ my $encoder = Email::MIME::RFC2047::Encoder->new(
+     encoding => $encoding,
+     method   => $method,
+ );
+
+Creates a new encoder object.
+
+I<encoding> specifies the encoding ("character set" in the RFC) to use. This is
+passed to the L<Encode> module. See L<Encode::Supported> for supported
+encodings.
+
+I<method> specifies the encoding method ("encoding" in the RFC). Must be either
+'B' or 'Q'.
+
+If both I<encoding> and I<method> are omitted, encoding defaults to 'utf-8'
+and method to 'Q'. If only I<encoding> is omitted it defaults to 'utf-8'.
+If only I<method> is omitted it defaults to 'B'.
+
+=head1 METHODS
+
+=head2 encode_text
+
+ my $encoded_text = $encoder->encode_text($string);
+
+Encodes a string that may replace a sequence of 'text' tokens (as defined by
+RFC 822) in any Subject or Comments header field, any extension message header
+field, or any MIME body part field for which the field body is defined as
+'*text'.
+
+This method tries to use the MIME encoding for as few characters of the
+input string as possible. So the result may consist of a mix of
+'encoded-words' and '*text'.
+
+The source string is trimmed and any whitespace is collapsed. The words in the
+result are separated by single space characters without folding of long lines.
+
+=head2 encode_phrase
+
+ my $encoded_phrase = $encoder->encode_phrase($string);
+
+Encodes a string that may replace a 'phrase' token (as defined by RFC 822),
+for example, one that precedes an address in a From, To, or Cc header.
+
+This method works like I<encode_text> but additionally converts remaining
+text that contains special characters to 'quoted-strings'.
+
+=head1 AUTHOR
+
+Nick Wellnhofer <wellnhofer@aevum.de>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) Nick Wellnhofer, 2009
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
+
